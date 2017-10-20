d677cabc5162120e919438bfa6d6c12d51a28777
Georgi Dimov <shogydimov@gmail.com>
2017-10-20 11:33:51 +0300

    edit request through ajax insted of form and add one more date validation

--- js/addPromoProduct.js
+++ js/addPromoProduct.js
@@ -1,3 +1,19 @@
+function getProduct() {
+    var getProduct = new XMLHttpRequest();
+    getProduct.onreadystatechange = function () {
+        if (this.readyState === 4){
+            if (this.status === 200){
+                var product = JSON.parse(this.responseText);
+                //TODO show the product number, img, brand, model, price
+                //TODO discuss what to get - productId or obj Product
+            }
+        }
+
+    };
+    getProduct.open("GET", "http://localhost/ittech/controller/addPromoProductController.php?getProduct");
+    getProduct.send();
+}
+
 function promoValidate() {
     var startDay = document.getElementById('startDay').value;
     var startMonth = document.getElementById('startMonth').value;
@@ -7,25 +23,70 @@ function promoValidate() {
     var endYear = document.getElementById('endYear').value;
     var discount = document.getElementById('discount').value;
     var discountWarning = document.getElementById('discountWarning');
-    var startDayWarning = document.getElementById('startDayWarning');
-    var endDayWarning = document.getElementById('endDayWarning');
-
+    var startDateWarning = document.getElementById('startDateWarning');
+    var endDateWarning = document.getElementById('endDateWarning');
+    var promoButton = document.getElementById('promoButton');
+    var error = false;
 
     if (startDay !== 'Day' && startMonth !== 'Month' && startYear !== 'Year'){
-        startDayWarning.innerText = '✔';
+        var startDate = startYear + '-' + startMonth + '-' + startDay;
+        startDateWarning.innerText = '✔';
     }else {
-        startDayWarning.innerText = '✘';
+        error = true;
+        startDateWarning.innerText = '✘';
     }
 
     if (endDay !== 'Day' && endMonth !== 'Month' && endYear !== 'Year'){
-        endDayWarning.innerText = '✔';
+        var endDate = endYear + '-' + endMonth + '-' + endDay;
+        endDateWarning.innerText = '✔';
     }else {
-        endDayWarning.innerText = '✘';
+        error = true;
+        endDateWarning.innerText = '✘';
     }
-
-    if (discount.length > 0 && discount.length < 45 && /\d/.test(discount)){
+    if (!error){
+        if (endDate > startDate){
+            endDateWarning.innerText = '✔';
+        }else {
+            error = true;
+            endDateWarning.innerText = 'The End date must be bigger than Start date!';
+        }
+    }
+    if (discount > 0 && discount.length > 0 && discount.length < 45 && /\d/.test(discount)){
         discountWarning.innerText = '✔';
     }else {
+        error = true;
         discountWarning.innerText = '✘';
     }
+    if(!error){
+        promoButton.onclick = function () {
+            //TODO add productId
+            sendPromoProduct(startDate, endDate, discount);
+        };
+    }else {
+        promoButton.onclick = false;
+    }
+
+}
+
+function sendPromoProduct(startDate, endDate, discount) {
+    var promoProduct = {
+        startDate: startDate,
+        endDate: endDate,
+        discount: discount
+        //TODO add productId;
+        // productId: productId
+    };
+     var json = JSON.stringify( promoProduct );
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+      if(this.readyState === 4){
+          if(this.status === 200){
+              alert(this.responseText);
+              document.getElementById('productInfo').innerText = 'Successfully added product in promotion';
+          }
+      }
+    };
+    request.open("POST","http://localhost/ittech/controller/addPromoProductController.php");
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.send('promoProduct='+json);
 }
