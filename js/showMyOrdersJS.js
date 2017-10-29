bb191a2ad2227f90099cbb98e9d2a2ab98eacfc2
ninaDimcheva <nkd.dimcheva@gmail.com>
2017-10-29 20:27:46 +0200

    added and completed page my orders for user

--- js/showMyOrdersJS.js
+++ js/showMyOrdersJS.js
@@ -1,9 +1,35 @@
 window.onload = function () {
+
     var myOrders = document.getElementById('myOrders');
     var userOrders = new XMLHttpRequest();
     userOrders.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            var userEmail = this.responseText;
+            var userOrders = JSON.parse(this.responseText);
+            var tableUserOrders = document.getElementById('tableUserOrders');
+            for(var i in userOrders){
+                var row = document.createElement('tr');
+                var numberOrder = document.createElement('td');
+                numberOrder.innerHTML = i;
+                row.appendChild(numberOrder);
+
+                var dateOrder = document.createElement('td');
+                dateOrder.innerHTML = userOrders[i].date;
+                row.appendChild(dateOrder);
+
+                var orderStatus = document.createElement('td');
+                orderStatus.innerHTML = userOrders[i].status;
+                row.appendChild(orderStatus);
+
+                var orderAddress = document.createElement('td');
+                orderAddress.innerHTML = userOrders[i].address;
+                row.appendChild(orderAddress);
+
+                var totalOrder = document.createElement('td');
+                totalOrder.innerHTML = userOrders[i].total;
+                row.appendChild(totalOrder);
+
+                tableUserOrders.appendChild(row);
+            }
         }
     };
     userOrders.open("POST","http://localhost/ittech/controller/showMyOrdersController.php");
--- js/showTypeTVJS.js
+++ js/showTypeTVJS.js
@@ -12,11 +12,10 @@ function showTypeTV() {
                 for(var i in types){
                     var list = document.createElement('li');
                     list.innerHTML = types[i];
-                    list.value = types[i];
                     list.className = 'types';
-                    list.onmouseover = function () {
-                        showBrandTV(this.innerText);
-                    };
+                    // list.onmouseover = function () {
+                    //     showBrandTV(this.innerText);
+                    // };
                     // list.onmouseout = function () {
                     //     hideBrandTV();
                     // };
@@ -40,39 +39,39 @@ function hideTypeTV(){
 }
 
 
-function showBrandTV(type){
-    var typeDiv = document.getElementById('tvTypes');
-    var  brandTV = document.getElementById('tvBrands');
-    var showBrandTV= new XMLHttpRequest();
-    showBrandTV.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            if (this.status === 200) {
-                var brandsForType = JSON.parse(this.responseText);
-                brandTV.innerHTML = '';
-                var brandList = document.createElement("ul");
-                for(var i in brandsForType){
-                        var list = document.createElement('li');
-                        list.innerHTML = brandsForType[i];
-                        list.value = brandsForType[i];
-                        list.className = 'brands';
-                        // list.onmouseover = function () {
-                        //     showBrandTV(this.value);
-                        // };
-                        // list.onmouseout = function () {
-                        //     hideBrandTV(this.value);
-                        // };
-                    var br = document.createElement('br');
-                    brandList.appendChild(br);
-                    brandList.appendChild(list);
-                    }
-                brandTV.appendChild(brandList);
-                typeDiv.appendChild(brandTV);
-            }
-        }
-    };
-    showBrandTV.open("GET", "http://localhost/ittech/controller/addProductController.php?getBrandsForType=" + type);
-    showBrandTV.send();
-}
+// function showBrandTV(type){
+//     var typeDiv = document.getElementById('tvTypes');
+//     var  brandTV = document.getElementById('tvBrands');
+//     var showBrandTV= new XMLHttpRequest();
+//     showBrandTV.onreadystatechange = function () {
+//         if (this.readyState === 4) {
+//             if (this.status === 200) {
+//                 var brandsForType = JSON.parse(this.responseText);
+//                 brandTV.innerHTML = '';
+//                 var brandList = document.createElement("ul");
+//                 for(var i in brandsForType){
+//                         var list = document.createElement('li');
+//                         list.innerHTML = brandsForType[i];
+//                         list.value = brandsForType[i];
+//                         list.className = 'brands';
+//                         // list.onmouseover = function () {
+//                         //     showBrandTV(this.value);
+//                         // };
+//                         // list.onmouseout = function () {
+//                         //     hideBrandTV(this.value);
+//                         // };
+//                     var br = document.createElement('br');
+//                     brandList.appendChild(br);
+//                     brandList.appendChild(list);
+//                     }
+//                 brandTV.appendChild(brandList);
+//                 typeDiv.appendChild(brandTV);
+//             }
+//         }
+//     };
+//     showBrandTV.open("GET", "http://localhost/ittech/controller/addProductController.php?getBrandsForType=" + type);
+//     showBrandTV.send();
+// }
 //
 // function hideBrandTV(){
 //     var brandDiv = document.getElementById('tvBrands');
